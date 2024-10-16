1d200925ef825a944dc1f52135da0fc699d6fe85
Ivan <ivan.georgiev2002k@gmail.com>
2024-10-16 21:51:18 +0200

    Adding data for the favorite movies to the profile page.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,9 +1,56 @@
-import React, { useState } from "react";
-import "./Profile.css"
+import React, { useEffect, useState } from "react";
+import "./Profile.css";
+import MoviePoster from "../../components/MoviePoster/MoviePoster";
+
 
 const Profile = () => {
 
-    const [userName, setUsername] = useState("Ivan")
+    const [userName, setUsername] = useState("Ivan");
+    const [favMovies, setFavMovies] = useState([]);
+    const favMoviesArr = [14160, 132344, 76, 80];
+    const [reRender, setReRender] = useState(1);
+
+    console.log(reRender, favMovies);
+
+    const fetchMovies = async (movieId) => {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzM3Mjg3ZDc2OTc3OWQwNGFiMDEzOGZmMGIwYjg4MCIsIm5iZiI6MTcyOTEwMTEzNi41MjM2NDMsInN1YiI6IjY0MTQ1NzE2YTZjMTA0MDA5YTAwM2QwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0kvRSO_ENVbuJgOzB91kpO0xsV62zwBky06sQgN1Pow'
+            }
+        };
+
+        const response = await fetch(url, options);
+        const json = await response.json();
+        console.log(json)
+
+        if (response.ok) {
+            setFavMovies(prevArr => {
+                const checkExists = prevArr.some(movie => movie.id === json.id);
+
+                if (!checkExists) {
+                    return [...prevArr, json]
+                } else {
+                    return prevArr
+                }
+
+
+            })
+        }
+
+    };
+
+    useEffect(() => {
+        favMoviesArr.map(id => {
+
+            fetchMovies(id)
+        });
+
+    }, [])
+
+
 
     return <div className="profile-page">
         <div className="profile-top">
